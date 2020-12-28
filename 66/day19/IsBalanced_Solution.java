39ddd70937b522c98c41282bd6a1c79f09841c34
ZiXiang Lin <linzixiang9983@gmail.com>
2020-12-29 00:37:11 +0800

    66 day19.2

--- 66/day19/IsBalanced_Solution.java
+++ 66/day19/IsBalanced_Solution.java
@@ -11,3 +11,17 @@ public class Solution {
         return 1 + Math.max(TreeDepth(root.left), TreeDepth(root.right));
     }
 }
+
+public class Solution {
+    public boolean IsBalanced_Solution(TreeNode root) {
+        return getDepth(root) != -1;
+    }
+    public int getDepth(TreeNode root) {
+        if(root == null) return 0;
+        int left = getDepth(root.left);
+        if(left == -1) return -1;
+        int right = getDepth(root.right);
+        if(right == -1) return -1;
+        return Math.abs(left - right) > 1 ? -1 : 1 + Math.max(left, right);
+    }
+}
